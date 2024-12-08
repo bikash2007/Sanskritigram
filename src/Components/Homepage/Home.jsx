913d2da0ab572d71337a996d7f20b9f0032e9d68
bikash2007 <bikashbhandari600@gmail.com>
2024-12-08 15:12:57 +0545

    ready to host

--- src/Components/Homepage/Home.jsx
+++ src/Components/Homepage/Home.jsx
@@ -13,20 +13,22 @@ const Home = () => {
         {
             name: 'Coming soon',
             photo: 'https://img.freepik.com/free-photo/top-view-meals-tasty-yummy-different-pastries-dishes-brown-surface_140725-14554.jpg?t=st=1733647251~exp=1733650851~hmac=db1c0eb894aba3617cea14be866db5be1bdf214f81f80d57f40707382b4cfe01&w=740',
-            link:'https://hansadhwani.sanskritigram.org/'
+            link:''
         },
         {
             name: 'coming soon',
             photo: 'https://img.freepik.com/free-vector/coming-soon-background-with-focus-light-effect-design_1017-27277.jpg?t=st=1733647371~exp=1733650971~hmac=881dc58c0a4f6c737b81bcf544f024ddfa0dfc217302922624f6ada6d64ba07b&w=740',
-            link:'https://hansadhwani.sanskritigram.org/'
+            link:''
         }
     ]
 
   return (
-      <div className='w-full h-screen flex gap-10 justify-center text-white items-center '>
+      <div className='w-full h-screen  flex gap-10 justify-center text-white items-center '>
+      <div class=" inset-0 -z-10 h-screen fixed w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+          
           {websites.map((items) => (
                   
-              <a href='https://hansadhwani.sanskritigram.org/' target='_main' className='h-96 w-72 relative rounded-lg flex items-end hover:scale-105 duration-200 active:scale-95 ' style={{background:`url(${items.photo})`,backgroundPosition:'center',backgroundSize:'cover'}}>
+              <a href={`${items.link}`} target='_main' className='h-96 w-72 relative rounded-lg flex items-end hover:scale-105 duration-200 active:scale-95 ' style={{background:`url(${items.photo})`,backgroundPosition:'center',backgroundSize:'cover'}}>
                     <div className='p-2'>
                       <h2 className='font-semibold'>{items.name}</h2>
                     <FontAwesomeIcon icon={faAngleRight} className='text-white z-10 absolute bottom-2 right-2'/>
